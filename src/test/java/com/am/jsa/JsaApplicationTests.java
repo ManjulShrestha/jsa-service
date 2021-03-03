e7fde7905737751b7ee805b257a75d454f85e751
Manjul Shrestha <manjul@manjul.manjul>
2021-03-02 17:39:31 -0700

    Addition of a test case

--- src/test/java/com/am/jsa/JsaApplicationTests.java
+++ src/test/java/com/am/jsa/JsaApplicationTests.java
@@ -1,13 +1,44 @@
 package com.am.jsa;
 
+import com.am.jsa.identity.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
-class JsaApplicationTests {
+@AutoConfigureMockMvc
+class JsaApplicationTests{
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testLogin() throws Exception{
+		String uri = "/service/v1/identity/authenticate";
+		User user = new User();
+		user.setUserName("manjulshr");
+		user.setPassword("manjul");
+
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(user)));
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
