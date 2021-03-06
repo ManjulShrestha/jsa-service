fc1eac2656b8f606be3609e8a7d1b26b1323ddeb
Manjul Shrestha <manjul@manjul.manjul>
2021-03-05 17:41:24 -0700

    File upload working

--- src/main/java/com/am/jsa/common/service/FileUploadService.java
+++ src/main/java/com/am/jsa/common/service/FileUploadService.java
@@ -3,31 +3,37 @@ package com.am.jsa.common.service;
 import com.am.jsa.common.SHAHash;
 import com.am.jsa.common.constants.UrlConstant;
 import com.am.jsa.common.nas.LocalNas;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.springframework.core.io.Resource;
 
 @Component
 public class FileUploadService {
 
 
+	public String uploadFile(MultipartFile file) {
 
-
-	public String uploadFile(FileUploadForm form,  String fileName) {
-
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 		String[] fileNameExt=fileName.split("\\.");
 		String fileNameHash=getHashedUniqueName(fileName);
 		try {
-			writeFile(form.getData(), UrlConstant.FILE_PATH+UrlConstant.UPLOAD_LOC+fileNameHash+"."+fileNameExt[1]);
+			writeFile(file.getBytes(), UrlConstant.FILE_PATH+UrlConstant.UPLOAD_LOC+fileNameHash+"."+fileNameExt[1]);
 			if(!fileNameExt[1].equalsIgnoreCase("pdf")) {
 				createThumbnail(fileNameHash, fileNameExt[1]);
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -60,28 +66,20 @@ public class FileUploadService {
 		// Get current date
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd H:m.s.S");
 		String currentTime = LocalDateTime.now().format(formatter);
-
 		// Get a unique string to hash: time, user, upload document type, uploaded filename & filesize
 		String uniqueString = currentTime + "@"
 				+ fileName;
-
-		// Hash the unique string
 		uniqueString = SHAHash.hash(uniqueString);
 
-		// append extension to the hashed string
-//		String extension = "pdf";
-//		if (!extension.isEmpty()) {
-//			uniqueString += "." + extension;
-//		}
 
 		return uniqueString;
 	}
 
 	public void createThumbnail(String fileName, String ext) throws IOException {
 		String fileCompletePath=UrlConstant.FILE_PATH+UrlConstant.UPLOAD_LOC+fileName+"."+ext;
-		BufferedImage img = ImageIO.read(new File(fileCompletePath));
+		//BufferedImage img = ImageIO.read(new File(fileCompletePath));
 		BufferedImage scaledImg =  new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-		//img.createGraphics().drawImage(ImageIO.read(new File(fileCompletePath)).getScaledInstance(100, 100, Image.SCALE_SMOOTH),0,0,null);
+		scaledImg.createGraphics().drawImage(ImageIO.read(new File(fileCompletePath)).getScaledInstance(100, 100, Image.SCALE_SMOOTH),0,0,null);
 		ImageIO.write(scaledImg, "jpg", new File(UrlConstant.FILE_PATH+UrlConstant.UPLOAD_LOC+"thumb_"+fileName+"."+ext));
 	}
 }
--- src/main/java/com/am/jsa/endpoints/FileUploadRestfulService.java
+++ src/main/java/com/am/jsa/endpoints/FileUploadRestfulService.java
@@ -2,15 +2,21 @@ package com.am.jsa.endpoints;
 
 
 import com.am.jsa.common.constants.UrlConstant;
-import com.am.jsa.common.service.FileUploadForm;
 import com.am.jsa.common.service.FileUploadService;
-
+import com.am.jsa.logger.AmLogger;
 import com.am.jsa.servicehandler.AmResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+
 @RestController
 @RequestMapping(UrlConstant.BASE_SERVICE_URL + UrlConstant.FILE_URL)
 public class FileUploadRestfulService {
@@ -19,12 +25,12 @@ public class FileUploadRestfulService {
     FileUploadService fileUploadService;
 
 
-//    @PostMapping("/upload")
-//    public ResponseEntity uploadFile(@MultipartForm FileUploadForm form,
-//                                     @RequestParam("fileName") String filename) {
-//        return AmResponse.successResponse(fileUploadService.uploadFile(form, filename));
-//
-//    }
+    @PostMapping("/upload")
+    public ResponseEntity uploadFile(
+            @RequestParam("name") MultipartFile file) {
+        return AmResponse.successResponse(fileUploadService.uploadFile(file));
+
+    }
 
     @PutMapping("delete-document/")
     public ResponseEntity deleteDocument(@RequestParam("fileName") String fileName) throws IOException{
@@ -32,20 +38,27 @@ public class FileUploadRestfulService {
         return AmResponse.successResponse("File Deleted");
     }
 
-//    @GetMapping("/get-uploaded-file")
-//    public ResponseEntity getFile(@RequestParam("fileName") String fileName) {
-//        File file = new File(UrlConstant.FILE_PATH + UrlConstant.UPLOAD_LOC + fileName);
-//        String contentType = "image/jpeg";
-//        String[] fileExt= fileName.split("\\.");
-//        if (fileExt[1].equalsIgnoreCase("pdf")){
-//              try{
-//            contentType = Files.probeContentType(file.toPath());
-//        }catch(Exception e){
-//            AmLogger.error(this.getClass(),e.getMessage());
-//        }
-//        }
-//
-//        return Response.ok(file)
-//                .header("Content-Type",contentType).build();
-//    }
+    @GetMapping("/get-uploaded-file")
+    public ResponseEntity getFile(@RequestParam("fileName") String fileName) throws Exception{
+        File file = new File(UrlConstant.FILE_PATH + UrlConstant.UPLOAD_LOC + fileName);
+        MediaType mediaType= MediaType.IMAGE_JPEG;
+        String[] fileExt= fileName.split("\\.");
+        if (fileExt[1].equalsIgnoreCase("pdf")){
+              try{
+                  mediaType = MediaType.APPLICATION_PDF;
+              }catch(Exception e){
+                 AmLogger.error(this.getClass(),e.getMessage());
+              }
+        }
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(mediaType)
+                .body(resource);
+    }
 }
